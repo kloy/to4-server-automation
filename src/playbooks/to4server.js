39f94fe8acf05325253aa1f226611d374d6fcd55
Keith Loy <kloy2@netspend.com>
2020-12-31 20:00:49 -0600

    Refactor phase 3

--- src/playbooks/to4server.js
+++ src/playbooks/to4server.js
@@ -1,22 +1,25 @@
 const path = require("path");
 const fs = require("fs-extra");
 const { promisify } = require("util");
-const scpClient = require("scp2");
 const { mkdirp, checkIfFileExists } = require("./ubuntu-fs");
 
-const INSTALL_PATH = '/home/to4adm/TO4Server';
+const INSTALL_PATH = "/home/to4adm/TO4Server";
 
 async function installTo4(ssh) {
     // Install the TO4Server
     console.log("installing TO4 Server");
     // Make sure TO4Server dir exists
     await mkdirp(ssh, INSTALL_PATH);
-    const result = await ssh.execCommand(
+    let result = await ssh.execCommand(
         `wget -q http://to4-dl.united-gameserver.de/packages/alpha/TOServer.sh -O - | bash`,
         {
             cwd: INSTALL_PATH,
         }
     );
+    console.log("STDOUT: " + result.stdout);
+    console.log("STDERR: " + result.stderr);
+    // Clear out default ini since we will need to copy one with our values applied
+    result = await ssh.execCommand(`rm ${INSTALL_PATH}/TO4cfg.ini`);
     console.log("STDOUT: " + result.stdout);
     console.log("STDERR: " + result.stderr);
 }
@@ -30,41 +33,37 @@ function buildIni(serverName, adminPassword) {
     const contentsWithValues = contents
         .replace("{{to4_server_name}}", serverName)
         .replace("{{to4_server_admin_password}}", adminPassword);
-    fs.ensureDirSync(path.resolve(__dirname, "../../build"));
-    const builtFilepath = path.resolve(__dirname, "../../build/TO4cfg.ini");
-    fs.writeFileSync(builtFilepath, contentsWithValues);
-    return builtFilepath;
+    return contentsWithValues;
 }
 
-async function copyIni(ssh, instanceIp, iniPath, user, password) {
-    console.log("Copying ini");
-    const destPath = `${INSTALL_PATH}/TO4cfg.ini`;
-    const result = await ssh.execCommand(
-        `rm ${destPath}`
-    );
-    console.log("STDOUT: " + result.stdout);
-    console.log("STDERR: " + result.stderr);
+async function sendIni(scpClient, content) {
+    console.log("Sending ini");
+    const destination = `${INSTALL_PATH}/TO4cfg.ini`;
 
-    const scp = promisify(scpClient.scp);
-    await scp(
-        iniPath,
-        `${user}:${password}@${instanceIp}:${destPath}`
-    );
+    const scp = promisify(scpClient.write.bind(scpClient));
+    await scp({
+        destination,
+        content: Buffer.from(content, 'utf8'),
+    });
     console.log("Ini copied");
 }
 
 const doesTo4Exist = async (ssh) =>
     await checkIfFileExists(ssh, `${INSTALL_PATH}/TOServer.sh`);
 
-async function install(ssh, { instanceIp, serverName, adminPassword, user, password }) {
+async function install({
+    ssh,
+    scpClient,
+    serverName = "TO4 Server",
+    adminPassword = "to4admin",
+}) {
     if (await doesTo4Exist(ssh)) {
         console.log("TO4 server already exists");
     } else {
         await installTo4(ssh);
+        const iniContent = buildIni(serverName, adminPassword);
+        await sendIni(scpClient, iniContent);
     }
-
-    const iniPath = buildIni(serverName, adminPassword);
-    await copyIni(ssh, instanceIp, iniPath, user, password);
 }
 
 module.exports = {
--- src/provisioners/to4server.js
+++ src/provisioners/to4server.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const { NodeSSH } = require("node-ssh");
+const { Client: ScpClient } = require("scp2");
 const { update, install } = require("../playbooks/apt-get");
 const { createUser } = require("../playbooks/ubuntu-user");
 const { install: installTo4 } = require("../playbooks/to4server");
@@ -32,26 +33,33 @@ async function connectTo4admSsh(instanceIp) {
     return to4Ssh;
 }
 
+function createTo4admScp(instanceIp) {
+    const to4ScpClient = new ScpClient({
+        host: instanceIp,
+        username: TO4_USER,
+        password: TO4_USER_PASSWORD,
+    });
+    return to4ScpClient;
+}
+
+
 async function main(instanceIp, serverName, adminPassword) {
     const rootSsh = await connectRootSsh(instanceIp);
     await update(rootSsh);
-    await install(rootSsh, ["p7zip-full, wget"]);
+    await install(rootSsh, ["p7zip-full", "wget"]);
     // Create a non root user (the server will NOT run as root user)
     await createUser(rootSsh, TO4_USER, TO4_USER_PASSWORD, "TO4Server Admin");
     rootSsh.dispose();
 
     const to4Ssh = await connectTo4admSsh(instanceIp);
-    await installTo4(to4Ssh, {
-        instanceIp,
+    const to4ScpClient = createTo4admScp(instanceIp);
+    await installTo4({
+        ssh: to4Ssh,
+        scpClient: to4ScpClient,
         serverName,
         adminPassword,
-        user: TO4_USER,
-        password: TO4_USER_PASSWORD,
     });
     to4Ssh.dispose();
-
-    // const rootSsh2 = await connectRootSsh(instanceIp);
-    // await startOnBoot(rootSsh2);
 }
 
 module.exports = main;
